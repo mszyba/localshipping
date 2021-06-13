daacb5e0d2ffa756596b924b9ae9ce820fd05862
Michał Szyba <m.szyba@gmail.com>
2021-06-13 11:58:50 +0200

    first commit

---
+++ src/main/java/eu/michalszyba/localshipping/LocalshippingApplication.java
@@ -0,0 +1,13 @@
+package eu.michalszyba.localshipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalshippingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalshippingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/michalszyba/localshipping/LocalshippingApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.michalszyba.localshipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalshippingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
